9257da6c2db9996e8c69eee4b141f020aaba4626
Andy Evans <andy@nology.io>
2021-12-16 10:55:03 +0000

    first commit

---
+++ src/main/java/com/nology/andydocker/SampleController.java
@@ -0,0 +1,7 @@
+package com.nology.andydocker;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+public class SampleController {
+}
---
+++ src/test/java/com/nology/andydocker/AndydockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nology.andydocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndydockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
